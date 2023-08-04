cad32edf0c0774eb6fef9121fe727d701ddfffd1
rolandoarellano69 <rolandoarellano9869@hotmail.com>
2023-08-04 15:41:15 -0700

    icon Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 import { RiCheckboxBlankCircleFill} from "react-icons/ri";
 const Header = () => {
   return (
-  <header className='flex w-full items-center p-4'>
-    <div className=' w-1/6 text-centert'>
-      <h1 className='text-xl font-bold'><RiCheckboxBlankCircleFill/>SES
-      <span className='text-2xl'>.</span>
-      </h1>
+  <header className='flex w-full items-center  p-4'>
+    <div className=' w-1/6 text-center'>
+      <span className='text-2xl font-bold relative tracking-widest	 bg-white p-1'>SES
+      <span className='text-5xl text-primary'>.</span>
+      <RiCheckboxBlankCircleFill className=' absolute text-primary -left-3 -bottom-3 -z-10'/>
+      </span>
     </div>
     <nav className='flex-1 text-center'>
       <h1>nav</h1>
